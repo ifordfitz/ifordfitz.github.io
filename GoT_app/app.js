bb7b66fc0e66cea3f30c60b6fc8466aeb0d468be
Ian Fitzgerald <ifordfitz@gmail.com>
2019-04-16 15:54:12 -0400

    valar morghulis added

--- GoT_app/app.js
+++ GoT_app/app.js
@@ -7,7 +7,32 @@ $(()=>{
         },
     );
 
-    $('.seasonOne').on('click', () => {
-      console.log('seasonOne clicked');
+    $('.seasonOne').on('click', (event) => {
+      event.preventDefault();
+      $('#ned').append($('<h3>valar morghulis</h3>'));
+      $('#ned').addClass('deadClass').removeClass('names');
+    })
+
+    $('.seasonThree').on('click', (event) => {
+      event.preventDefault();
+      $('#cTully').append($('<h3>valar morghulis</h3>'));
+      $('#cTully').addClass('deadClass').removeClass('names');
+      $('#robby').append($('<h3>valar morghulis</h3>'));
+      $('#robby').addClass('deadClass').removeClass('names');
+    })
+
+    $('.seasonFive').on('click', (event) => {
+      event.preventDefault();
+      $('#jSnow').append($('<h3>valar morghulis</h3>'));
+      $('#jSnow').addClass('deadClass').removeClass('names');
+    })
+
+    $('.seasonSix').on('click', (event) => {
+      event.preventDefault();
+      $('#jSnow').children().remove();
+      $('#jSnow').append($('<h2>Jon Snow</h2>'));
+      $('#jSnow').addClass('names').removeClass('deadClass');
+      $('#ricky').append($('<h3>valar morghulis</h3>'));
+      $('#ricky').addClass('deadClass').removeClass('names');
     })
 })
