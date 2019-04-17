ab87053e45c63bc620091a76aa82475b8822d5a6
Ian Fitzgerald <ifordfitz@gmail.com>
2019-04-17 15:24:30 -0400

    season one overlay for ned and cat

--- GoT_app/app.js
+++ GoT_app/app.js
@@ -1,4 +1,8 @@
 $(()=>{
+
+    ///////////////////
+    // AJAX
+    ///////////////////
     $.ajax({
         url:'https://www.anapioficeandfire.com/api/houses/362'
     }).then(
@@ -7,12 +11,29 @@ $(()=>{
         },
     );
 
+
+    ///////////////////
+    // SEASON ONE
+    ///////////////////
     $('.seasonOne').on('click', (event) => {
       event.preventDefault();
       $('#ned').append($('<h3>valar morghulis</h3>'));
       $('#ned').addClass('deadClass').removeClass('names');
+      // $('#ned').hover(showNedOne, hideNedOne);
     })
 
+
+    ///////////////////
+    // SEASON TWO
+    ///////////////////
+    $('.seasonTwo').on('click', (event) => {
+      event.preventDefault();
+
+    })
+
+    ///////////////////
+    // SEASON THREE
+    ///////////////////
     $('.seasonThree').on('click', (event) => {
       event.preventDefault();
       $('#cTully').append($('<h3>valar morghulis</h3>'));
@@ -21,12 +42,26 @@ $(()=>{
       $('#robby').addClass('deadClass').removeClass('names');
     })
 
+
+    ///////////////////
+    // SEASON FOUR
+    ///////////////////
+
+
+
+    ///////////////////
+    // SEASON FIVE
+    ///////////////////
     $('.seasonFive').on('click', (event) => {
       event.preventDefault();
       $('#jSnow').append($('<h3>valar morghulis</h3>'));
       $('#jSnow').addClass('deadClass').removeClass('names');
     })
 
+
+    ///////////////////
+    // SEASON SIX
+    ///////////////////
     $('.seasonSix').on('click', (event) => {
       event.preventDefault();
       $('#jSnow').children().remove();
@@ -35,4 +70,32 @@ $(()=>{
       $('#ricky').append($('<h3>valar morghulis</h3>'));
       $('#ricky').addClass('deadClass').removeClass('names');
     })
+
+
+    ///////////////////
+    // SEASON SEVEN
+    ///////////////////
+
+
+
+    ///////////////////
+    // SEASON EIGHT
+    ///////////////////
+
 })
+
+
+///////////////////
+// OVERLAY TEXT
+///////////////////
+
+// SEASON ONE
+
+// EDDARD STARK
+// const showNedOne = (event) => {
+//   $(event.currentTarget).css('z-index', 1).text('Beheaded in Kings Landing');
+// }
+//
+// const hideNedOne = (event) => {
+//
+// }
