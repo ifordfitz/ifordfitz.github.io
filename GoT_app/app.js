57f1320912f721f27fc227294ac9e171f6461e75
Ian Fitzgerald <ifordfitz@gmail.com>
2019-04-17 23:45:59 -0400

    season two text added but button not working

--- GoT_app/app.js
+++ GoT_app/app.js
@@ -22,7 +22,7 @@ $(()=>{
       $('#nedText').text("Beheaded in King's Landing on the orders of King Joffrey");
       $('#catText').text("Kidnaps Tyrion at the Crossroads Inn, takes him to the Eyrie, meets Robb's army in the Riverlands");
       $('#robbText').text("Marches the Stark army south and wins a battle against the Lannister army");
-      $('#jonText').text("Joins the nights watch");
+      $('#jonText').text("Joins the nights watch, makes friends and enemies");
       $('#sansaText').text("Betrothed to Joffrey, is generally uppity, regrets wanting to go to King's Landing");
       $('#aryaText').text("Bit of a tomboy, learns to sword fight, escapes King's Landing with Hot Pie and co");
       $('#branText').text("Pushed out a window and is paralyzed, starts having Three-Eyed Raven dreams");
@@ -35,7 +35,14 @@ $(()=>{
     ///////////////////
     $('.seasonTwo').on('click', (event) => {
       event.preventDefault();
-
+      $('#nedText').text("Still missing a head");
+      $('#catText').text("Releases Jaime Lannister and warns Robb not to break his marriage promise to House Frey");
+      $('#robbText').text("Continues to be victorious against the Lannister forces, falls in love with Talisa");
+      $('#jonText').text("Ranges beyond the wall, meets Ygritte, and is captured by the Lord of Bones");
+      $('#sansaText').text("Stuck in King's Landing, is saved by The Hound, Joffrey divorces her");
+      $('#aryaText').text("Somehow becomes Tywin Lannister's cupbearer and befriends Jaqen H'ghar");
+      $('#branText').text("Escapes Winterfell after Theon's betrayal and heads north with Hodor, Osha, Rickon, and direwolves x 2");
+      $('#rickonText').text("Escapes Winterfell after Theon's betrayal and heads north with Hodor, Osha, Bran, and direwolves x 2");
     })
 
     ///////////////////
